6950df757139ef2fbb25ffb74f13615b27536f88
Rigdon Windsor <rigdon.windsor@gmail.com>
2025-02-03 09:49:53 -0500

    Delete Users/Junior/Desktop directory

--- Users/Junior/Desktop/BPA Practice test/debugging.py
+++
@@ -1,7 +0,0 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-    #in the original it was print(factorial[5]) the brackets wouldnt work with this so it needed parentheses instead.
-print(factorial(5))
--- Users/Junior/Desktop/BPA Practice test/manipulation.py
+++
@@ -1,5 +0,0 @@
-def reverse_and_capitalize(string):
-   #::-1 makes it reverse and the .upper just means to uppercase it
-    return string[::-1].upper()
-
-print(reverse_and_capitalize("rigdon windsor"))
